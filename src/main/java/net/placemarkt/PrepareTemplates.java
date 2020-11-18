fb436f7d926679c92a3214b4a2f852a5500d9e30
rkoeze <rkoeze@gmail.com>
2020-11-17 16:14:09 -0800

    Get basic post-install logic working

---
+++ src/main/java/net/placemarkt/PrepareTemplates.java
@@ -0,0 +1,8 @@
+package net.placemarkt;
+
+public class PrepareTemplates {
+
+  public static void main(String[] args) {
+    System.out.println("######### I am working I swear to you!!!!!!!!!!!!");
+  }
+}
