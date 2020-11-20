ea1348a64f7b62181689778901d726b203768560
rkoeze <rkoeze@gmail.com>
2020-11-20 10:49:14 -0800

    Get abbreviations from all abbreviations files

--- src/main/java/net/placemarkt/TemplateProcessor.java
+++ src/main/java/net/placemarkt/TemplateProcessor.java
@@ -5,9 +5,12 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class TemplateProcessor {
 
@@ -22,7 +25,16 @@ public class TemplateProcessor {
     try (PrintWriter out = new PrintWriter("src/main/resources/templates/templates.json")) {
       out.println(json);
     }
-    System.out.println(json);
+  }
+
+  private static void transpileAliases() throws IOException {
+    Path path = Paths.get("address-formatting/conf/components.yaml");
+    String yaml = Files.readString(path);
+    Object obj = yamlReader.readValue(yaml, Object.class);
+    String json = jsonWriter.writeValueAsString(obj);
+    try (PrintWriter out = new PrintWriter("src/main/resources/templates/aliases.json")) {
+      out.println(json);
+    }
   }
 
   private static void transpileCountry2Lang() throws IOException {
@@ -33,13 +45,84 @@ public class TemplateProcessor {
     try (PrintWriter out = new PrintWriter("src/main/resources/templates/country2Lang.json")) {
       out.println(json);
     }
-    System.out.println(json);
+  }
+
+  private static void transpileCountryCodes() throws IOException {
+    Path path = Paths.get("address-formatting/conf/country_codes.yaml");
+    String yaml = Files.readString(path);
+    Object obj = yamlReader.readValue(yaml, Object.class);
+    String json = jsonWriter.writeValueAsString(obj);
+    try (PrintWriter out = new PrintWriter("src/main/resources/templates/countryCodes.json")) {
+      out.println(json);
+    }
+  }
+
+  private static void transpileStateCodes() throws IOException {
+    Path path = Paths.get("address-formatting/conf/state_codes.yaml");
+    String yaml = Files.readString(path);
+    Object obj = yamlReader.readValue(yaml, Object.class);
+    String json = jsonWriter.writeValueAsString(obj);
+    try (PrintWriter out = new PrintWriter("src/main/resources/templates/stateCodes.json")) {
+      out.println(json);
+    }
+  }
+
+  private static void transpileAbbreviations() throws IOException {
+    Object abbreviations = new Object();
+    ObjectReader objectReader = yamlReader.readerForUpdating(abbreviations);
+    try (Stream<Path> paths = Files.list(Paths.get("address-formatting/conf/abbreviations"))) {
+      ObjectNode json = jsonWriter.createObjectNode();
+      paths.forEach(path -> {
+        System.out.println(path);
+        try {
+          String yaml = Files.readString(path);
+          Object obj = yamlReader.readValue(yaml, Object.class);
+          ObjectNode node = jsonWriter.valueToTree(obj) ;
+          json.setAll(node);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+
+      });
+      String jsonString = jsonWriter.writeValueAsString(json);
+      try (PrintWriter out = new PrintWriter("src/main/resources/templates/abbreviations.json")) {
+        out.println(jsonString);
+      }
+      //paths.filter(Files::isRegularFile).collect(Collectors.toSet());
+    }
+
+    //Path path = Paths.get("address-formatting/conf/abbreviations");
+    //String yaml = Files.readString(path);
+    //Object obj = yamlReader.readValue(yaml, Object.class);
+    //String json = jsonWriter.writeValueAsString(obj);
+    //try (PrintWriter out = new PrintWriter("src/main/resources/templates/abbreviations.json")) {
+    //  out.println(json);
+    //}
+  }
+
+  private static void transpileCountryNames() throws IOException {
+    Path path = Paths.get("address-formatting/conf/country_codes.yaml");
+    String yaml = Files.readString(path);
+    String formattedYaml = yaml.replaceAll(" \\# ", " ");
+    System.out.println(formattedYaml);
+    Object obj = yamlReader.readValue(formattedYaml, Object.class);
+    String json = jsonWriter.writeValueAsString(obj);
+    try (PrintWriter out = new PrintWriter("src/main/resources/templates/countryNames.json")) {
+      out.println(json);
+    }
   }
 
   public static void main(String[] args) {
     try {
-      TemplateProcessor.transpileWorldwide();
-      TemplateProcessor.transpileCountry2Lang();
+      //DONE
+      //TemplateProcessor.transpileWorldwide();
+      //TemplateProcessor.transpileCountryNames();
+      //TODO
+      TemplateProcessor.transpileAbbreviations();
+      //TemplateProcessor.transpileAliases();
+      //TemplateProcessor.transpileCountry2Lang();
+      //TemplateProcessor.transpileCountryCodes();
+      //TemplateProcessor.transpileStateCodes();
     } catch (Exception e){
       e.printStackTrace();
     }
