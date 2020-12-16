056ab9b1663b0ef3bc13c486f877f52a42ba26d6
rkoeze <rkoeze@gmail.com>
2020-12-16 10:51:08 -0800

    Add abbreviation tests

--- src/test/java/net/placemarkt/AddressFormatterTest.java
+++ src/test/java/net/placemarkt/AddressFormatterTest.java
@@ -113,16 +113,16 @@ public class AddressFormatterTest {
 
     @Test
     public void correctlySetsFallbackCountryCode() throws Exception {
-      String json = "{city: 'Antwerp',"
-          + "city_district: 'Antwerpen',"
-          + "country: 'Belgium',"
-          + "country_code: 'yu',"
-          + "county: 'Antwerp',"
-          + "house_number: 63,"
-          + "neighbourhood: 'Sint-Andries',"
-          + "postcode: 2000,"
-          + "restaurant: 'Meat & Eat',"
-          + "road: 'Vrijheidstraat',"
+      String json = "{city: 'Antwerp',\n"
+          + "city_district: 'Antwerpen',\n"
+          + "country: 'Belgium',\n"
+          + "country_code: 'yu',\n"
+          + "county: 'Antwerp',\n"
+          + "house_number: 63,\n"
+          + "neighbourhood: 'Sint-Andries',\n"
+          + "postcode: 2000,\n"
+          + "restaurant: 'Meat & Eat',\n"
+          + "road: 'Vrijheidstraat',\n"
           + "state: 'Flanders'}";
       String formatted = formatter.format(json, "US");
       assertEquals(formatted, "Meat & Eat\n"
@@ -133,34 +133,105 @@ public class AddressFormatterTest {
 
     @Test
     public void correctlyAppendsCountry() throws Exception {
-      String json = "{\"houseNumber\": 301,\n"
-          + "  \"road\": \"Hamilton Avenue\",\n"
-          + "  \"neighbourhood\": \"Crescent Park\",\n"
-          + "  \"city\": \"Palo Alto\",\n"
-          + "  \"postcode\": 94303,\n"
-          + "  \"county\": \"Santa Clara County\",\n"
-          + "  \"state\": \"California\",\n"
-          + "  \"countryCode\": \"US\",}";
+      String json = "{houseNumber: '301',\n"
+          + "road: 'Hamilton Avenue',\n"
+          + "neighbourhood: 'Crescent Park',\n"
+          + "city: 'Palo Alto',\n"
+          + "postcode: '94303',\n"
+          + "county: 'Santa Clara County',\n"
+          + "state: 'California',\n"
+          + "countryCode: 'US',}";
       String formatted = formatterWithAppendCountryFlag.format(json);
       assertEquals("301 Hamilton Avenue\n"
           + "Palo Alto, CA 94303\n"
           + "United States of America\n", formatted);
     }
 
-    //@Test
-    //public void correctlyAbbreviatesAddresses() throws Exception {
-    //  String json = "{\"houseNumber\": 301,\n"
-    //      + "  \"road\": \"Hamilton Avenue\",\n"
-    //      + "  \"neighbourhood\": \"Crescent Park\",\n"
-    //      + "  \"city\": \"Palo Alto\",\n"
-    //      + "  \"postcode\": 94303,\n"
-    //      + "  \"county\": \"Santa Clara County\",\n"
-    //      + "  \"state\": \"California\",\n"
-    //      + "  \"countryCode\": \"US\",}";
-    //  String formatted = formatterWithAbbreviationFlag.format(json);
-    //  assertEquals("301 Hamilton Avenue\n"
-    //      + "Palo Alto, CA 94303\n"
-    //      + "United States of America\n", formatted);
-    //}
+    @Test
+    public void correctlyAbbreviatesAvenue() throws Exception {
+      String json = "{country_code: 'US',\n"
+          + "house_number: '301',\n"
+          + "road: 'Hamilton Avenue',\n"
+          + "neighbourhood: 'Crescent Park',\n"
+          + "city: 'Palo Alto',\n"
+          + "postcode: '94303',\n"
+          + "county: 'Santa Clara County',\n"
+          + "state: 'California',\n"
+          + "country: 'United States',}";
+      String formatted = formatterWithAbbreviationFlag.format(json);
+      assertEquals("301 Hamilton Ave\n"
+          + "Palo Alto, CA 94303\n"
+          + "United States of America\n", formatted);
+    }
+
+
+    @Test
+    public void correctlyAbbreviatesRoad() throws Exception {
+      String json = "{country_code: 'US',\n"
+          + "house_number: '301',\n"
+          + "road: 'Northwestern University Road',\n"
+          + "neighbourhood: 'Crescent Park',\n"
+          + "city: 'Palo Alto',\n"
+          + "postcode: '94303',\n"
+          + "county: 'Santa Clara County',\n"
+          + "state: 'California',\n"
+          + "country: 'United States'}";
+      String formatted = formatterWithAbbreviationFlag.format(json);
+      assertEquals("301 Northwestern University Rd\n"
+          + "Palo Alto, CA 94303\n"
+          + "United States of America\n", formatted);
+    }
+
+    @Test
+    public void doesNotAbbreviateWhenComponentNotPresent() throws Exception {
+      String json = "{country_code: 'US',\n"
+          + "house_number: '301',\n"
+          + "neighbourhood: 'Crescent Park',\n"
+          + "city: 'Palo Alto',\n"
+          + "postcode: '94303',\n"
+          + "county: 'Santa Clara County',\n"
+          + "state: 'California',\n"
+          + "country: 'United States'}";
+      String formatted = formatterWithAbbreviationFlag.format(json);
+      assertEquals("301\n"
+          + "Palo Alto, CA 94303\n"
+          + "United States of America\n", formatted);
+    }
+
+    @Test
+    public void correctlyAbbreviatesCAAvenue() throws Exception {
+      String json = "{city: 'Vancouver',\n"
+          + "country: 'Canada',\n"
+          + "country_code: 'ca',\n"
+          + "county: 'Greater Vancouver Regional District',\n"
+          + "postcode: 'V6K',\n"
+          + "road: 'Cornwall Avenue',\n"
+          + "state: 'British Columbia',\n"
+          + "suburb: 'Kitsilano',}";
+      String formatted = formatterWithAbbreviationFlag.format(json);
+      assertEquals("Cornwall Ave\n"
+          + "Vancouver, BC V6K\n"
+          + "Canada\n", formatted);
+    }
+
+
+    @Test
+    public void correctlyAbbreviatesESCarrer() throws Exception {
+      String json = "{city: 'Barcelona',\n"
+          + "city_district: 'Sarrià - Sant Gervasi',\n"
+          + "country: 'Spain',\n"
+          + "country_code: 'es',\n"
+          + "county: 'BCN',\n"
+          + "house_number: '68',\n"
+          + "neighbourhood: 'Sant Gervasi',\n"
+          + "postcode: '08017',\n"
+          + "road: 'Carrer de Calatrava',\n"
+          + "state: 'Catalonia',\n"
+          + "suburb: 'les Tres Torres'}";
+      String formatted = formatterWithAbbreviationFlag.format(json);
+      assertEquals("C Calatrava, 68\n"
+          + "08017 Barcelona\n"
+          + "Spain\n", formatted);
+    }
   }
 }
