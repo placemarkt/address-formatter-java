ffbea26924cf8a1af93cdcd1299242b5bb731ba2
rkoeze <rkoeze@gmail.com>
2020-12-31 18:05:53 -0800

    Remove import

--- src/main/java/net/placemarkt/Templates.java
+++ src/main/java/net/placemarkt/Templates.java
@@ -2,7 +2,6 @@ package net.placemarkt;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 import java.io.InputStream;
 
